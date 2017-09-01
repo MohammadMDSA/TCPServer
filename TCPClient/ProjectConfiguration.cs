d77ffff0d8d84583ce691dbaecf0986a1c7af9bc
Mohammad MDSA <samimd.77@gmail.com>
2017-09-01 20:04:13 +0430

    Add Client setting view and fixed MainPage's view minor bugs

---
+++ TCPClient/ProjectConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCPClient.Pages;
+using TCPServer;
+
+namespace TCPServer
+{
+	public partial class MainPage
+	{
+		public const string APP_TITLE = "TCP Client";
+
+		private List<Scenario> Scenarios = new List<Scenario>()
+		{
+			new Scenario() {PageType = typeof(Setting), Icon="\uE115", Title="Setting"}
+		};
+	}
+}
